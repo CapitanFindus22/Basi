f1d13574ef013c9b36bcdc6e520b2577a80d49f5
CapitanFindus <106431916+CapitanFindus22@users.noreply.github.com>
2024-07-30 19:23:42 +0200

    Update C.c

--- C.c
+++ C.c
@@ -9,184 +9,197 @@ const float PI = 3.14;
 
 /*Basic data types:
 
-    Data Types	            Memory Size	        Range 
+	Data Types	            Memory Size	        Range 
     
-    char	              1 byte	        −128 to 127                
-    unsigned char	      1 byte	        0 to 255
+    	char	              1 byte	        −128 to 127                
+  	unsigned char	      1 byte	        0 to 255
 
-    short	              2 byte	     	−32,768 to 32,767
-    unsigned short	      2 byte	       	0 to 65,535
+    	short	              2 byte	     	−32,768 to 32,767
+    	unsigned short	      2 byte	       	0 to 65,535
 
-    int	                      2 byte	     	−32,768 to 32,767
-    unsigned int	      2 byte	       	0 to 65,535
-    long int	              4 byte 		-2,147,483,648 to 2,147,483,647
-    unsigned long int	      4 byte	   	0 to 4,294,967,295
+    	int	                      2 byte	     	−32,768 to 32,767
+    	unsigned int	      2 byte	       	0 to 65,535
+    	long int	              4 byte 		-2,147,483,648 to 2,147,483,647
+    	unsigned long int	      4 byte	   	0 to 4,294,967,295
 
-    float	              4 byte		1.2E-38 to 3.4E+38 		6 decimal places
-    double	              8 byte		2.3E-308 to 1.7E+308		15 decimal places
-    long double	             10 byte		3.4E-4932 to 1.1E+4932		19 decimal places
+    	float	              4 byte		1.2E-38 to 3.4E+38 		6 decimal places
+    	double	              8 byte		2.3E-308 to 1.7E+308		15 decimal places
+    	long double	             10 byte		3.4E-4932 to 1.1E+4932		19 decimal places
 
-    bool                      1 byte <-- need stdbool.h
+    	bool                      1 byte <-- need stdbool.h
+*/
+
+/*Cast:
+
+	To change one variable from one type to another
+
+    	Ex. float to int  ->  	int a = 0;
+    			  	float b = 1.1;
+	 		  	a = (int)b;
+    			 
+    	In this case the value will be truncated
+
 */
 
 /*Arrays:
 
-    -int x[5];
+    	- int x[5];
 
-    -int x[] = {1,2,3}
+    	- int x[] = {1,2,3}
 
 */
 
 /*Matrices:
 
-    -int x[2][2];
+    	- int x[2][2];
 
-    -int x[][] = {
-		{1,2,3},
-                  {4,5,6}
-                 };
+    	- int x[][] = 	{
+			{1,2,3},
+                  	{4,5,6}
+			};
 
 */
 
 /*Format specifiers:
 
-     %c    	// char
-     %s    	// character array
-     %f    	// float
-     %lf   	// double
-     %d    	// int,unsigned short,short,(bool,char,unsigned char) < - as numbers
-     %u    	// unsigned int
-     %lld  	// long long int
-     %llu  	// unsigned long long int
+     	%c    	// char
+     	%s    	// character array
+     	%f    	// float
+     	%lf   	// double
+     	%d    	// int,unsigned short,short,(bool,char,unsigned char) < - as numbers
+     	%u    	// unsigned int
+     	%lld  	// long long int
+     	%llu  	// unsigned long long int
 
-     %p    	// address
+     	%p    	// address
 
-     %.1~  	// decimal precision
-     %-~   	// left align
-     %1~   	// min field width
+     	%.number~  	// decimal precision
+     	%-~   	// left align
+     	%number~   	// min field width
 */
 
 /*Logical operators:
 
-    && 	// and
-    || 	// or
-    !~ 	// not
+    	&& 	// and
+    	|| 	// or
+    	! 	// not
 
 */
 
 /*Bitwise operators:
 
-    &// and
-    |  	// or
-    ^  	// xor
-    << 1	// left shift 1 posizione, se fatta su numero unsigned è num*(2^1)
-    >> 1	// right shift 1 pos., come sopra ma num/(2^1)
+    	&	// and
+    	|  	// or
+    	^  	// xor
+    	<< 1	// left shift 1 position, if done on an unsigned is equal to num*(2^1)
+    	>> 1	// right shift 1 pos., if done on an unsigned is equal to num/(2^1)
 
 */
 
 /*Control flow:
 
-    1) Conditions:
+	1) Conditions:
 
-        - if(){}
+        	- if(){}
 
-        - if(){}
-          else{}
+        	- if(){}
+          	  else{}
 
-        - if(){}
-          else if{}
-          else{}
+       	 	- if(){}
+          	  else if{}
+          	  else{}
 
-        - switch()
-          {
-            case 'A': 
-            something;
-            break;
+        	- switch()
+          	{
+            		case 'A': 
+            		something;
+            		break;
 
-            default:
-            something;           
+            		default:
+            		something;           
 
-          }
+	        }
 
-        - ternary operator (condition)? do_this_if_true : do_this_if_false ;
+	        - ternary operator (condition)? do_this_if_true : do_this_if_false ;
 
-    2) Loops:
+    	2) Loops:
 
-        - for(int i=0;i < 5;i++){}
+        	- for(int i=0;i < 5;i++){}
 
-        - while(len < 10){len++;}
+        	- while(len < 10){len++;}
 
-        - do{}while(i%2 == 0);
+        	- do{}while(i%2 == 0);
 
-        break      	// exit the loop
-        continue   	// skip to next iteration
+       	 	break      	// exit the loop
+        	continue   	// skip to next iteration
 */
 
 /*Structs:
 
-    struct Square
-    {
-        int origin_x;
-        int origin_y;
-        int side;
-    };
+    	struct Square
+    	{
+        	int origin_x;
+        	int origin_y;
+        	int side;
+    	};
 
-struct Square x;
-x.side = 4;
+	struct Square x;
+	x.side = 4;
 
 */
 
 /*Unions:
 
-    union number
-    {
-        short small;
-        long big;
-        double really_big;
-    };
+    	union number
+    	{
+       	 	short small;
+        	long big;
+        	double really_big;
+    	};
 
-Can contain all of the declared values but only one at a time
+	Can contain all of the declared values but only one at a time
 */
 
 /*Enums:
 
- enum weekday {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+ 	enum weekday {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
 
 To create a set of constants
 */
 
 /*Typedef:
 
-    - on a struct or union
+    	- on a struct or union
 
-        typedef struct 
-        {
-            int origin_x;
-            int origin_y;
-            int side;
-        }Square;
+        	typedef struct 
+        	{
+            		int origin_x;
+            		int origin_y;
+           	 	int side;
+	      
+        	}Square;
 
-        Square x;
+        	Square x;
 
-    - in general -> typedef char s25[25];
+    	- in general -> typedef char s25[25];
 
         s25 x; a string of 25 chars
 */
 
 //Pointers: int *pX = &x;
-// To access members of struct with a pointer use x->member
-// An array can be accessed also as a pointer
+// 	To access members of struct with a pointer use x->member
+// 	An array can be accessed also as a pointer
 
 //Function prototype
-// int double(int);
+// 	int double(int);
 
 //Files
 void foo()
 {
 	/*
-	    w = write
-	    a = append
-	    r = read
+	    	w = write
+	    	a = append
+	    	r = read
 
 	*/
 	FILE *pF = fopen("a.txt", "w");
@@ -217,6 +230,7 @@ void foo()
 void foo2()
 {
 	// array of 20 ints
+	// (int*) is a cast from void* to int*
 	int *ptr = (int*) malloc(20* sizeof(int));
 
 	// array of 20 ints set to 0
